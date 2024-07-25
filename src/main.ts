0895ce9345dc8b20905bdf749a48f5b437f5c8fe
Jaimi Patel <jaimip.albiorix@gmail.com>
2024-07-25 10:53:22 +0530

    add : vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
 
 import App from './App.vue'
 import router from './router'
+
+import 'vuetify/styles'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
